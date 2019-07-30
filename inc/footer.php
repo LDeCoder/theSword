acffbf2c6292999b2161606d80d6d19b17e6bf9e
Lori Davis <loridavis@Loris-MBP.home.gateway>
2019-07-30 17:56:00 -0500

    home page completed - project off the ground

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>| This site is created and maintained by LDeCoder &copy; 2019 |  github: LDeCoder </p>
+    </footer>
+    
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+
+    <title>PHP website</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">LDeCoder</a>
+  <ul>
+        <li><a href='index.php'>HOME</a></li>
+        <li><a href='about.php'>ABOUT</a></li>
+        <li><a href='contact.php'>CONTACT</a></li>
+    </ul>
+</nav>
+    <ul>
+        <li><a href='index.php'>HOME</a></li>
+        <li><a href='about.php'>ABOUT</a></li>
+        <li><a href='contact.php'>CONTACT</a></li>
+    </ul>
