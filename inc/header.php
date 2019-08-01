953771812fa0f2fd694d23dc8186c4b298e66e23
LDeCoder <loridavis@Loris-MBP.home.gateway>
2019-08-01 10:40:37 -0500

    updates to html and header for future  php

--- inc/header.php
+++ inc/header.php
@@ -13,6 +13,7 @@
   <a class="navbar-brand" href="#">LDeCoder</a>
   <ul>
         <li><a href='index.php'>HOME</a></li>
+        
         <li><a href='about.php'>ABOUT</a></li>
         <li><a href='contact.php'>CONTACT</a></li>
     </ul>
